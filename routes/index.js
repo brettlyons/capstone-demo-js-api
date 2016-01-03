677017ee91a57da3722d873a0297935378113870
Brett Lyons <blyons@students.naropa.edu>
2016-01-02 17:14:37 -0700

    rm'd users routes, added /api/times/:x/:y routes, also /api/echo route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/api/echo', function(req, res, next) {
+  console.log("POST RECEIVED REQ.BODY.THINGIE: ", req.body.thingie);
+  res.json(req.body.thingie); // from the clj api docs -- "echoes a Thingie from json-body"
+});
+
+router.get('/api/times/:x/:y', function(req, res, next) {
+  res.json(req.params.x * req.params.y);
+});
 module.exports = router;
